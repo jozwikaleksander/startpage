741b4eb3ceedb90bb1307edbbb8f20ff323bc816
Aleksander Jozwik <aleksanderjozwik@elektronik.edu.pl>
2022-10-03 21:12:20 +0200

    Getting focus on the search bar instead of address bar

--- scripts/bookmarks.js
+++ scripts/bookmarks.js
@@ -133,3 +133,15 @@ $(document).keyup(function(e) {
         }
    }
 });
+
+if (location.search !== "?_") {
+    location.search = "?_";
+    throw new Error; // load everything on the next page;
+    // stop execution on this page
+}
+
+function onLoad() {
+    document.getElementById("search").focus();
+}
+
+window.onload = onLoad;
